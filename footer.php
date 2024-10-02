fb6103ef2b6f4af8f664a05c0026b4c9a0217ae3
Alvaro Parra <petromoneda@gmail.com>
2024-10-02 04:38:38 -0400

    se crea footer.php y se enlaza a index.php con la funcion get_footer

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer class="container-fluid py-4 text-center bg-dark text-secondary" data-bs-theme="dark">
+        <h5>Lorem ipsum dolor sit amet consectetur.</h5>
+    </footer>
+    
+<?php wp_footer();?>
+</body>
+</html>

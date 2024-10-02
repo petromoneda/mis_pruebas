565ce085709e0f557b1ef2398f3450fc510f45d6
Alvaro Parra <petromoneda@gmail.com>
2024-10-02 04:41:19 -0400

    se crea header.php y se enlaza a index.php mediante la funcion get_header

---
+++ header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Lab de Física</title>
+    <?php wp_head(); ?>
+
+</head>
+<body>
+    <!-------------------------------------------- Menu------------------------------->
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Laboratorio de Física</a>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="#">Blog</a>
+                    <a class="nav-link" href="#">Contacto</a>
+                </div>
+                <form class="position-absolute end-0 me-2 d-flex" role="search">
+                    <input class="form-control" type="search" placeholder="Search" aria-label="Search"/>
+                    <button class="btn btn-outline-success" type="submit"> Search </button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+    <!--------------------------------------------Fin Menu------------------------------->
+    <!---------------------------------------Blog---------------------------------------->
+    <div class="container" data-bs-theme="dark">
+        <h2 class="my-5 text-center text-primary">Mi Blog personal</h2>
+        <hr>

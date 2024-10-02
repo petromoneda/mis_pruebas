b33d715bb80f61f852099a0c6f17ac5b854e5db6
Alvaro Parra <petromoneda@gmail.com>
2024-10-02 02:18:03 -0400

    se creo functions.php agregando los style css y script con sus acciones

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+function agregar_css_js(){
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+    wp_enqueue_style( 'bootstrap_solar', get_template_directory_uri() . '/css/bootstrap.min.css', false, '1.1', 'all');
+    wp_enqueue_script('boostrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js', array( 'jquery' ),'5.3.3',true);
+}
+add_action( 'wp_enqueue_scripts', 'agregar_css_js' );
+?>
